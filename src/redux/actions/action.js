de5ed16cb58ed0c1397d620fb47f5eaaac167d36
Abhishekkumar4444 <ojhaababaara@gmail.com>
2022-12-18 01:47:40 +0530

    first commit

---
+++ src/redux/actions/action.js
@@ -0,0 +1,25 @@
+import { ADD_CART, RMV_CART, RMV_ONE } from "../constants/ActionTypes";
+
+export const ADD = (item) => {
+  return {
+    type: ADD_CART,
+    payload: item,
+  };
+};
+
+// remove iteams
+export const DLT = (id) => {
+  return {
+    type: RMV_CART,
+    payload: id,
+  };
+};
+
+// remove individual iteam
+
+export const REMOVE = (iteam) => {
+  return {
+    type: RMV_ONE,
+    payload: iteam,
+  };
+};
